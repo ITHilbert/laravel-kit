41de32c06af6ea3ef682a54af067d8e777362b25
Jörg Hilbert <hilbert@it-entwickler-hilbert.de>
2021-08-31 16:42:03 +0200

    mehr Vue

--- src/Publish/Resources/js/vueapp.js
+++ src/Publish/Resources/js/vueapp.js
@@ -28,15 +28,34 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 
+//Buttons
+Vue.component('hbutton', require('@ithilbert/jlib/src/vue/buttons/HButton.vue').default);
+Vue.component('button-create', require("@ithilbert/jlib/src/vue/buttons/button-create.vue").default);
+Vue.component('button-show', require('@ithilbert/jlib/src/vue/buttons/button-show.vue').default);
+Vue.component('button-edit', require('@ithilbert/jlib/src/vue/buttons/button-edit.vue').default);
+Vue.component('button-delete', require('@ithilbert/jlib/src/vue/buttons/button-delete.vue').default);
+Vue.component('button-save', require('@ithilbert/jlib/src/vue/buttons/button-save.vue').default);
+Vue.component('button-back', require('@ithilbert/jlib/src/vue/buttons/button-back.vue').default);
+Vue.component('button-submit', require('@ithilbert/jlib/src/vue/buttons/button-submit.vue').default);
+Vue.component('button-cancel', require('@ithilbert/jlib/src/vue/buttons/button-cancel.vue').default);
+Vue.component('button-reset', require('@ithilbert/jlib/src/vue/buttons/button-reset.vue').default);
 
-//Design
-Vue.component('card', require("@ithilbert/jlib/src/vue/Card.vue").default);
-Vue.component('hform', require('@ithilbert/jlib/src/vue/HForm.vue').default);
-Vue.component('group', require('@ithilbert/jlib/src/vue/group.vue').default);
+//Card
+Vue.component('card-body', require("@ithilbert/jlib/src/vue/card/card-body.vue").default);
+Vue.component('card-columns', require("@ithilbert/jlib/src/vue/card/card-columns.vue").default);
+Vue.component('card-deck', require("@ithilbert/jlib/src/vue/card/card-deck.vue").default);
+Vue.component('card-footer', require("@ithilbert/jlib/src/vue/card/card-footer.vue").default);
+Vue.component('card-group', require("@ithilbert/jlib/src/vue/card/card-group.vue").default);
+Vue.component('card-header', require("@ithilbert/jlib/src/vue/card/card-header.vue").default);
+Vue.component('card-img-bottom', require("@ithilbert/jlib/src/vue/card/card-img-bottom.vue").default);
+Vue.component('card-img-top', require("@ithilbert/jlib/src/vue/card/card-img-top.vue").default);
+Vue.component('card-text', require("@ithilbert/jlib/src/vue/card/card-text.vue").default);
+Vue.component('card-title', require("@ithilbert/jlib/src/vue/card/card-title.vue").default);
+Vue.component('card', require("@ithilbert/jlib/src/vue/card/card.vue").default);
+//Vue.component('jCard', require("@ithilbert/jlib/src/vue/card/jCard.vue").default);
 
-//Comobobox
-Vue.component('combobox', require('@ithilbert/jlib/src/vue/input/combobox.vue').default);
-Vue.component('search', require('@ithilbert/jlib/src/vue/input/search.vue').default);
+//Dialoge
+Vue.component('dialog-delete', require('@ithilbert/jlib/src/vue/dialog/delete.vue').default);
 
 //Input
 Vue.component('input-text', require('@ithilbert/jlib/src/vue/input/input-text.vue').default);
@@ -54,17 +73,9 @@ Vue.component('input-hidden', require('@ithilbert/jlib/src/vue/input/input-hidde
 Vue.component('hlabel', require('@ithilbert/jlib/src/vue/input/hlabel.vue').default);
 //Vue.component('editor', require('./editor.vue').default);
 
-//Buttons
-Vue.component('hbutton', require('@ithilbert/jlib/src/vue/buttons/HButton.vue').default);
-Vue.component('button-create', require("@ithilbert/jlib/src/vue/buttons/button-create.vue").default);
-Vue.component('button-show', require('@ithilbert/jlib/src/vue/buttons/button-show.vue').default);
-Vue.component('button-edit', require('@ithilbert/jlib/src/vue/buttons/button-edit.vue').default);
-Vue.component('button-delete', require('@ithilbert/jlib/src/vue/buttons/button-delete.vue').default);
-Vue.component('button-save', require('@ithilbert/jlib/src/vue/buttons/button-save.vue').default);
-Vue.component('button-back', require('@ithilbert/jlib/src/vue/buttons/button-back.vue').default);
-Vue.component('button-submit', require('@ithilbert/jlib/src/vue/buttons/button-submit.vue').default);
-Vue.component('button-cancel', require('@ithilbert/jlib/src/vue/buttons/button-cancel.vue').default);
-Vue.component('button-reset', require('@ithilbert/jlib/src/vue/buttons/button-reset.vue').default);
+//Comobobox
+Vue.component('combobox', require('@ithilbert/jlib/src/vue/input/combobox.vue').default);
+Vue.component('search', require('@ithilbert/jlib/src/vue/input/search.vue').default);
 
 
 //Show
@@ -72,9 +83,24 @@ Vue.component('txt', require('@ithilbert/jlib/src/vue/show/show-text.vue').defau
 Vue.component('euro', require('@ithilbert/jlib/src/vue/show/show-euro.vue').default);
 Vue.component('datum', require('@ithilbert/jlib/src/vue/show/show-date.vue').default);
 
-//Dialoge
-Vue.component('dialog-delete', require('@ithilbert/jlib/src/vue/dialog/delete.vue').default);
+//SVG
+Vue.component('svg-pen', require('@ithilbert/jlib/src/vue/svg/pen.vue').default);
+Vue.component('svg-star', require('@ithilbert/jlib/src/vue/svg/star.vue').default);
 
+//Tabs
+Vue.component('tab-content', require('@ithilbert/jlib/src/vue/tabs/tab-content.vue').default);
+Vue.component('tab-header', require('@ithilbert/jlib/src/vue/tabs/tab-header.vue').default);
+Vue.component('tab-iteam', require('@ithilbert/jlib/src/vue/tabs/tab-iteam.vue').default);
+Vue.component('tab-panel', require('@ithilbert/jlib/src/vue/tabs/tab-panel.vue').default);
+
+
+//View
+Vue.component('rating', require('@ithilbert/jlib/src/vue/view/rating.vue').default);
+
+
+//Design
+Vue.component('hform', require('@ithilbert/jlib/src/vue/HForm.vue').default);
+Vue.component('group', require('@ithilbert/jlib/src/vue/group.vue').default);
 
 
 /**
