0763c80a17c94a19c76f56114535688afbb9fcc9
Jörg Hilbert <hilbert@it-entwickler-hilbert.de>
2021-09-14 19:48:50 +0200

    add VueComboBox

---
+++ src/Entities/ComboBox.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ITHilbert\LaravelKit\Entities;
+
+//use Illuminate\Database\Eloquent\Model;
+
+class ComboBox //extends Model
+{
+
+    public $cbKey;
+    public $cbCaption;
+
+}
