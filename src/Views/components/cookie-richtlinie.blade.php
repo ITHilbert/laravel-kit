a34f9dd6aa140d44bc430980a343470017274379
Jörg Hilbert <hilbert@it-entwickler-hilbert.de>
2024-03-20 11:12:27 +0100

    add law Components

---
+++ src/Views/components/cookie-richtlinie.blade.php
@@ -0,0 +1,8 @@
+<div>
+    @if($cookieInhalt)
+        {!! $cookieInhalt['content'] !!}
+    @else
+        <iframe src="https://center.it-hilbert.com/show/cookie-richtlinie" width="800px" height="500px"></iframe>
+        <a href="https://center.it-hilbert.com/show/cookie-richtlinie" target="_blank">Cookie-Richtlinie in neuem Fenster öffnen</a>
+    @endif
+</div>
